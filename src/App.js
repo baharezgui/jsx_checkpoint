ae9c68af6f2c786af27f81efe87c55ca92bc633d
baharezgui <bahaeddinerezgui29@gmail.com>
2024-02-27 02:18:14 +0100

    checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://mk-media.mytek.tn/media/catalog/product/cache/8be3f98b14227a82112b46963246dfe1/p/c/pc-de-bureau-gamer-mytek-i5-12e-gen-16go-msi-rtx-3050-ventus-2x-6g-oc.jpg" />
+      <Card.Body>
+        <Card.Title>PC DE BUREAU GAMER MYTEK I5 12È GÉN 16GO MSI RTX 3050 VENTUS 2X 6G OC</Card.Title>
+        <Card.Text>
+        Processeur: Intel Core i5-12400F (2.50 GHz up to 4.40 GHz Turbo max, 18Mo Mémoire cache, Hexa-Core) - Système d'exploitation: FreeDos - Mémoire RAM: ADATA 16Go DDR4 3200 Mhz Udimm - Disque Dur: MSI SSD SPATIUM 240Go S270 SATA 2.5" - Carte mère: MSI PRO H610M-E DDR4 mATX - Carte graphique: MSI GeForce RTX 3050 VENTUS 2X 6G OC - Boîtier gamer MSI MAG FORGE M100A / Micro-ATX / 4 ventilo Auto RGB - Alimentation MSI MAG A500DN 500W 80+WHITE - Dimensions: 388 x 200 x 423 mm - Couleur: Noir - Garantie: 1 an
+        </Card.Text>
+        <Button variant="primary">1889</Button>
+      </Card.Body>
+    </Card>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,9 @@
+const products = {
+    name: "PC DE BUREAU GAMER MYTEK I5 12È GÉN 16GO MSI RTX 3050 VENTUS 2X 6G OC",
+    description: "Processeur: Intel Core i5-12400F (2.50 GHz up to 4.40 GHz Turbo max, 18Mo Mémoire cache, Hexa-Core) - Système d'exploitation: FreeDos - Mémoire RAM: ADATA 16Go DDR4 3200 Mhz Udimm - Disque Dur: MSI SSD SPATIUM 240Go S270 SATA 2.5\" - Carte mère: MSI PRO H610M-E DDR4 mATX - Carte graphique: MSI GeForce RTX 3050 VENTUS 2X 6G OC - Boîtier gamer MSI MAG FORGE M100A / Micro-ATX / 4 ventilo Auto RGB - Alimentation MSI MAG A500DN 500W 80+WHITE - Dimensions: 388 x 200 x 423 mm - Couleur: Noir - Garantie: 1 an",
+    price: 1889,
+    imgURL: "https://mk-media.mytek.tn/media/catalog/product/cache/8be3f98b14227a82112b46963246dfe1/p/c/pc-de-bureau-gamer-mytek-i5-12e-gen-16go-msi-rtx-3050-ventus-2x-6g-oc.jpg",
+  };
+  
+  export default products;
+  
